bc7a5bad0632e1b94b55931eab633da56003631f
DaBlincx <80812233+DaBlincx@users.noreply.github.com>
2022-01-26 14:51:50 +0100

    added stuff

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from rich.panel import *
 from rich.progress import *
 from rich.console import *
 import pyfiglet
+from fontlist import *
 
 con = Console()
 
@@ -16,10 +17,17 @@ def clscr():
         print()
 
 def mainMenu():
-    clscr()
-    banner = pyfiglet.figlet_format("    Cryptix",font="xtty")
-    con.print(banner,style="bold green")
+    ##clscr()
+    print("\n\n")
+    print(f"{nr}/{len(fontlist)}\n{fontch}")
+    banner = pyfiglet.figlet_format("Cryptix",font=fontch)
+    f_banner = con.print(banner,style="bold green")
+    time.sleep(0.01)
+    f.write(f"\n\n{nr}/{len(fontlist)}\n{fontch}\n{f_banner}")
 
-
-
-mainMenu()
+nr = 0
+f = open("printfonts.txt","w",encoding="UTF-8")
+for fontch in fontlist:
+    nr += 1
+    mainMenu()
+f.close
